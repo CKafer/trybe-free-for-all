0dc0aa09653a107d5dde522714f8e2ed9430cfd9
CKafer <claudiokafer@hotmail.com>
2022-09-23 15:25:49 -0300

    - Inicio

---
+++ array-and-loops.js
@@ -0,0 +1,11 @@
+const lotteryNumbers = [14, 25, 35, 46, 43, 36];
+
+console.log(lotteryNumbers);
+console.log(lotteryNumbers[0]);
+console.log(lotteryNumbers[1]);
+console.log(lotteryNumbers[2]);
+console.log(lotteryNumbers[3]);
+console.log(lotteryNumbers[4]);
+console.log(lotteryNumbers[5]);
+
+console.log(lotteryNumbers.length);
