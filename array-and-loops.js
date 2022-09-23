68ab921b06d8a0a26fa20d1d612e5431b1aac04d
CKafer <claudiokafer@hotmail.com>
2022-09-23 15:41:00 -0300

    - Estrutura com FOR OF

--- array-and-loops.js
+++ array-and-loops.js
@@ -1,11 +1,22 @@
 const lotteryNumbers = [14, 25, 35, 46, 43, 36];
 
-console.log(lotteryNumbers);
-console.log(lotteryNumbers[0]);
-console.log(lotteryNumbers[1]);
-console.log(lotteryNumbers[2]);
-console.log(lotteryNumbers[3]);
-console.log(lotteryNumbers[4]);
-console.log(lotteryNumbers[5]);
+// Estrutura sem FOR
+//console.log(lotteryNumbers);
+//console.log(lotteryNumbers[0]);
+//console.log(lotteryNumbers[1]);
+//console.log(lotteryNumbers[2]);
+//console.log(lotteryNumbers[3]);
+//console.log(lotteryNumbers[4]);
+//console.log(lotteryNumbers[5]);
 
-console.log(lotteryNumbers.length);
+//console.log(lotteryNumbers.length);
+
+// Estrutura com FOR
+//for (let index = 0; index < lotteryNumbers.length; index += 1) {
+//  console.log(lotteryNumbers[index]);
+//}
+
+// Estrutura com FOR OF
+for (let value of lotteryNumbers) {
+  console.log(value);
+} 
