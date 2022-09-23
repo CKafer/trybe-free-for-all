4b37a9024461e95b2faa59a446e11e77a2a48940
CKafer <claudiokafer@hotmail.com>
2022-09-23 16:41:17 -0300

    - Commit inicial

---
+++ fastFoodApp.js
@@ -0,0 +1,7 @@
+// A primeira funcionalidade é a geração do cardápio! que deve ter o formato "ITEM - R$ PREÇO".
+
+let items = ['Batata Frita', 'Hambúrguer', 'CheeseBurguer', 'Refrigerante', 'Nuggets'];
+let prices = [4, 12, 14, 6, 12];
+
+
+console.log(items[0]+" = R$ "+prices[0]);
