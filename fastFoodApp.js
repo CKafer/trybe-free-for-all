0cf6cb9b13c470494de9e243dd8e93a4441b1e6b
CKafer <claudiokafer@hotmail.com>
2022-09-23 16:59:50 -0300

    - Primeiro intervalo da aula

--- fastFoodApp.js
+++ fastFoodApp.js
@@ -1,11 +1,14 @@
 // A primeira funcionalidade é a geração do cardápio! que deve ter o formato "ITEM - R$ PREÇO".
 
-let items = ['Batata Frita', 'Hambúrguer', 'CheeseBurguer', 'Refrigerante', 'Nuggets'];
+let items = ['Batata Frita', 'Hambúrguer', 'CheeseBurguer', 'Refrigerante', 'Nuggets', 'Suco'];
 let prices = [4, 12, 14, 6, 12];
-
 
 //console.log(items[0]+" = R$ "+prices[0]);
 
 for ( let index = 0; index < items.length; index += 1 ) {
-  console.log(items[index]+" = R$ "+prices[index]);
+  if ( prices[index] == undefined) {
+    console.log(items[index]+" = Preço Indefinido");
+  } else {
+    console.log(items[index]+" = R$ "+prices[index]);
+  }
 }
